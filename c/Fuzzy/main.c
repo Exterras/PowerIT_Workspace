44985d0d47114ba1a8a37ce0a66ba4901e3be0ce
asdf <asdf@asdf.com>
2017-02-28 16:38:52 +0900

    20170228 16:38:00

--- c/Fuzzy/main.c
+++ c/Fuzzy/main.c
@@ -10,8 +10,8 @@
 #include <stdlib.h>
 
 // function prototype
-void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _highFuzzy_LowNum); // (fuzzy's degree, )
-void fuzzyPrint(char* _str, double* _arrFuzzyValue, int _numLength); // fuzzy print function
+void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _num_HighZero); // (fuzzy's degree, )
+void fuzzyPrint(char* _str, double* _arrFuzzyValue, int _numLength, int _line); // fuzzy print function
 
 // main
 int main(){
@@ -20,47 +20,45 @@ int main(){
     int numLength = sizeof(num) / sizeof(num[0]);
 
     // execute functions
-    fuzzyOn(num, numLength, 100, 0.5, 666);
+    fuzzyOn(num, numLength, 100, 0.5, 600);
     return 0;
 }
 
 // function of bubble sort
-void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _highFuzzy_LowNum){
+void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _num_HighZero){
     // general variables
     int* num = _num;
-    int i, j, temp; // bubble sort variables
-    int highNum; // array highest number
-    int lowNum; // array lowest number
-    int numSum = 0; // number array sum
-    double numAvg = 0.0; // number array avg
+    int i, j, temp; // bubble sort and iteration variables
+    int sum_Entire = 0; // array's sum
+    double avg_Entire = 0.0; // array's avg
     double avgRate = _avgRate;
 
-    // fuzzy variables
+    // data variables
     int numLength = _numLength;
-    int lowFuzzy_HighNum = 0;
-    int highFuzzy_LowNum = _highFuzzy_LowNum;
+    int num_High; // array highest number, high 1.0's number
+    int num_Low; // array lowest number, low 1.0's number
+    int num_LowLast = 0; // low last in an array
+    int num_HighZero = _num_HighZero; // high 0.0's number
+    int num_LowZero = 0; // // low 0.0's number
 
-    double avgFuzzyNum = 0.0;
+    double num_FuzzyAvgInLow = 0.0; // avg 0.0's number -> 1.0
+    double num_FuzzyAvgInHigh = 0.0; // 1.0 -> avg 0.0's number
+    double avg_FuzzyAvg = 0.0; // avg 1.0's number
 
-    // fuzzy standard measure
+    // fuzzy measurement variables
     int degree = _degree;
-    // double numFuzzyLowZero = 0.0;
-
     double fuzzyLow = 0.0;
     double fuzzyHigh = 0.0;
     double fuzzyLowToAvg = 0.0;
     double fuzzyAvgToHigh = 0.0;
+    double fuzzy_AvgInLow = avgRate;
+    double fuzzy_AvgInHigh = avgRate;
 
-    double avgFuzzyLow = avgRate;
-    double avgFuzzyHigh = avgRate;
-
-    double num_LowfuzzyRate = 0.0;
-    double num_HighfuzzyRate = 0.0;
-
-    double arrFuzzyLow[numLength];
-    double arrFuzzyHigh[numLength];
-    double arrFuzzyLowToAvg[numLength];
-    double arrFuzzyAvgToHigh[numLength];
+    // fuzzy array variables
+    double arr_FuzzyLow[numLength];
+    double arr_FuzzHigh[numLength];
+    double arr_FuzzyLowToAvg[numLength];
+    double arr_FuzzyAvgToHigh[numLength];
 
     // before bubble sort, initialize value
     printf("initialize value :\t");
@@ -71,19 +69,17 @@ void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _highF
 
 
 
-
-
     /* ----- bubble sort, maxvalue, minvalue, avgvalue -----*/
 
     // highest, lowest value
-    highNum = num[0];
-    lowNum = num[0];
+    num_High = num[0];
+    num_Low = num[0];
     for(i = 0; i < numLength; i++){
-        if(num[i] > highNum){
-            highNum = num[i];
+        if(num[i] > num_High){
+            num_High = num[i];
         }
-        if(num[i] < lowNum){
-            lowNum = num[i];
+        if(num[i] < num_Low){
+            num_Low = num[i];
         }
     }
 
@@ -109,16 +105,16 @@ void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _highF
     for(i = numLength-1 ; i >= 0; i--){
         printf("%d ",num[i]);
     }
-    printf("\n"); // carriage return
+    printf("\n\n"); // carriage return
 
     // num[]'s average
     for(i = 0; i < numLength; i++){
-        numSum += num[i];
+        sum_Entire += num[i];
     } // iteration of number array adding
 
-    numAvg = (double)numSum / (double)numLength; // number array average method
-    printf("number's sum :\t\t%d\n", numSum);
-    printf("number's avg :\t\t%.2lf\n", numAvg); //.2 = decimal point
+    avg_Entire = (double)sum_Entire / (double)numLength; // number array average method
+    printf("number's sum :\t\t%d\n", sum_Entire);
+    printf("number's avg :\t\t%.2lf\n\n", avg_Entire); //.2 = decimal point
 
 
 
@@ -126,59 +122,102 @@ void fuzzyOn(int *_num, int _numLength, int _degree, double _avgRate, int _highF
 
     // low fuzzy 중 가장 키가 큰 변수를 골라내는 과정
     for (i = 0; i < numLength; i++) {
-        if (num[i] < numAvg) {
-            lowFuzzy_HighNum = num[i];
+        if (num[i] < avg_Entire) {
+            num_LowLast = num[i];
         }
     }
 
     // 사용자가 설정한 degree 나누기 각 소속 집단별로 최대값과 최소값를 구하여 그 차액 -> 전체 집단에서 한 눈금이 차지하는 비율
-    fuzzyLow = (double)degree / (double)(lowFuzzy_HighNum - lowNum);
-    fuzzyHigh = (double)degree / (double)(highNum - highFuzzy_LowNum);
+    fuzzyLow = (double)degree / (double)(num_LowLast - num_Low);
+    fuzzyHigh = (double)degree / (double)(num_High - num_HighZero);
 
-    num_LowfuzzyRate = (double)lowFuzzy_HighNum - ((avgFuzzyLow * degree) / fuzzyLow);
+    num_FuzzyAvgInLow = (double)num_LowLast - ((fuzzy_AvgInLow * degree) / fuzzyLow);
     // Low 집단 중 Average 집단에 소속될 수 있는 최소한의 데이터
     // low 집단 중 가장 큰 데이터  - (low 집단 중 평균으로 속할 수 있는 소속도 * degree) / low 집단전체 한 눈금이 차지하는 비율
 
-    num_HighfuzzyRate = ((avgFuzzyHigh * degree) / fuzzyHigh) + (double)highFuzzy_LowNum;
+    num_FuzzyAvgInHigh = ((fuzzy_AvgInHigh * degree) / fuzzyHigh) + (double)num_HighZero;
     // High 집단 중 Average 집단에 소속될 수 있는 최대한의 데이터 ==
     // (high 집단 중 평균으로 속할 수 있는 소속도 * degree) / low 집단에서 한 눈금이 차지하는 비율 + 큰 데이터를 판별하는 기준 데이터
 
-    avgFuzzyNum = (num_HighfuzzyRate + num_LowfuzzyRate) / 2.0;
-    fuzzyLowToAvg = (double)degree / (avgFuzzyNum - num_LowfuzzyRate);
-    fuzzyAvgToHigh = (double)degree / (num_HighfuzzyRate - avgFuzzyNum);
+    avg_FuzzyAvg = (num_FuzzyAvgInHigh + num_FuzzyAvgInLow) / 2.0;
+    fuzzyLowToAvg = (double)degree / (avg_FuzzyAvg - num_FuzzyAvgInLow);
+    fuzzyAvgToHigh = (double)degree / (num_FuzzyAvgInHigh - avg_FuzzyAvg);
 
     // 각 집단별 fuzzy 구하기
     for (i = 0; i < numLength; i++) {
         // 각 집단들의 퍼지 논리 (0~1 사이의 실수값) = (눈금자 * (최대범위 - 최소범위)) / degree
         // Fuzzy 값의 범위 = 0.0 ~ 1.0
-        arrFuzzyLow[i] = (fuzzyLow * (double)(lowFuzzy_HighNum - num[i])) / (double)degree;
-        arrFuzzyHigh[i] = (fuzzyHigh * (double)(num[i] - highFuzzy_LowNum)) / (double)degree;
-        arrFuzzyLowToAvg[i] = (fuzzyLowToAvg * (double)(num[i] - num_LowfuzzyRate)) / (double)degree;
-        arrFuzzyAvgToHigh[i] = (fuzzyAvgToHigh * (double)(num_HighfuzzyRate - num[i])) / (double)degree;
+        arr_FuzzyLow[i] = (fuzzyLow * (double)(num_LowLast - num[i])) / (double)degree;
+        arr_FuzzHigh[i] = (fuzzyHigh * (double)(num[i] - num_HighZero)) / (double)degree;
+        arr_FuzzyLowToAvg[i] = (fuzzyLowToAvg * (double)(num[i] - num_FuzzyAvgInLow)) / (double)degree;
+        arr_FuzzyAvgToHigh[i] = (fuzzyAvgToHigh * (double)(num_FuzzyAvgInHigh - num[i])) / (double)degree;
     }
 
     // fuzzy print - low
-    fuzzyPrint("Low Fuzzy :\t\t", arrFuzzyLow, numLength);
+    for (i = 0; i < numLength; i++) {
+        if(arr_FuzzyLow[i] == 0.0){
+            num_LowZero = num[i];
+            break;
+        }
+    } // low fuzzy가 0.0인 데이터를 num_LowZero에 넣고 break
+    fuzzyPrint("Low Fuzzy :\t\t", arr_FuzzyLow, numLength, 1);
 
     // fuzzy print - high
-    fuzzyPrint("High Fuzzy :\t\t", arrFuzzyHigh, numLength);
+    fuzzyPrint("High Fuzzy :\t\t", arr_FuzzHigh, numLength, 1);
 
     // fuzzy print - average
-    fuzzyPrint("LowToAvg Fuzzy :\t", arrFuzzyLowToAvg, numLength);
-    fuzzyPrint("AvgToHigh Fuzzy :\t", arrFuzzyAvgToHigh, numLength);
+    fuzzyPrint("LowToAvg Fuzzy :\t", arr_FuzzyLowToAvg, numLength, 2);
+    fuzzyPrint("AvgToHigh Fuzzy :\t", arr_FuzzyAvgToHigh, numLength, 2);
+
+    // fuzzy decision
+
+    // num_FuzzyAvgInLow : Low 집단 중 Average 집단에 소속될 수 있는 최소한의 데이터
+    // num_FuzzyAvgInHigh :  High 집단 중 Average 집단에 소속될 수 있는 최대한의 데이터
+    // num_HighZero : High 집단 fuzzy 0.0일때의 데이터
+    // num_LowZero : Low 집단 fuzzy 0.0일때의 데이터
+    // num_Low : 전체 집단 중 가장 작은 데이터
+
+    printf("fuzzy decision:\t\t");
+    for (i = 0; i < numLength; i++) {
+        if (num[i] > num_FuzzyAvgInHigh) {
+            printf("TALL ");
+        } else if (num[i] >= num_HighZero && num[i] < num_FuzzyAvgInHigh) {
+            printf("ATAL ");
+        } else if (num[i] >= num_LowZero && num[i] < num_HighZero) {
+            printf("AVRG ");
+        } else if (num[i] >= num_FuzzyAvgInLow && num[i] < num_LowZero) {
+            printf("AVSL ");
+        } else if (num[i] >= num_Low) {
+            printf("SMAL ");
+        }
+    }
+
 }
 
-void fuzzyPrint(char* _str, double* _arrFuzzyValue, int _numLength){
+void fuzzyPrint(char* _str, double* _arrFuzzyValue, int _numLength, int _line){
     int i;
 
     printf("%s", _str);
     for(i = 0; i < _numLength; i++){
-        if(_arrFuzzyValue[i] > 0.0 && _arrFuzzyValue[i] < 1.0){
-            printf("%.2lf ", _arrFuzzyValue[i]);
+        if(_line == 1){
+            if(_arrFuzzyValue[i] > 0.0){
+                printf("%.2lf ", _arrFuzzyValue[i]);
+            } else {
+                printf("%.2lf ", 0.0);
+            }
         } else {
-            printf("%.2lf ", 0.0);
+            if(_arrFuzzyValue[i] > 0.0 && _arrFuzzyValue[i] < 1.0){
+                printf("%.2lf ", _arrFuzzyValue[i]);
+            } else if (_arrFuzzyValue[i] < 0.0){
+                printf("%.2lf ", 0.0);
+            } else if (_arrFuzzyValue[i] == 1.0){
+                printf("%.2lf ", 1.0);
+            } else {
+                printf("VOID ");
+            }
         }
     // printf("%.2lf ", arrFuzzyValue[i]);
     }
     printf("\n");
 }
+
